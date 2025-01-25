9e2a7212675f62e4d2f652bdde4401330537ec6d
AlessandroRiva <alessandrovittorioriva@gmail.com>
2025-01-25 00:06:55 -0800

    fixed year bug in footer. adjusted date formatting to IT dd-MMMM-yyyy

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/output.css");
+  eleventyConfig.addPassthroughCopy("./src/index.js");
+
+  // get the current year for copyright date
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Add filter to format date in Italian locale
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj)
+      .setLocale("it") // Set locale explicitly to Italian
+      .toFormat("dd MMMM yyyy"); // Format as medium date
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
